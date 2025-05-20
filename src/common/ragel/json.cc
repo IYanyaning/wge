7a0fd611368a1acadf0f8ccd2595baf1acbf85f0
zhouyu <zhouyu-jt@163.com>
2025-05-20 19:05:34 +0800

    feat(common): add ragel json parser

---
+++ src/common/ragel/json.cc
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2024-2025 Stone Rhino and contributors.
+ *
+ * MIT License (http://opensource.org/licenses/MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include "json.h"
+
+#include <json.h>
+
+namespace Wge {
+namespace Common {
+namespace Ragel {
+void Json::init(std::string_view json_str) {
+  key_value_map_.reserve(32);
+  key_value_linked_.reserve(32);
+  parseJson(json_str, key_value_map_, key_value_linked_);
+}
+} // namespace Ragel
+} // namespace Common
+} // namespace Wge
