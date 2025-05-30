ba0c4c3710c55c99a3273595d857efbe8611eac1
zhouyu <zhouyu-jt@163.com>
2025-05-30 09:38:08 +0800

    fix(test): remove unnecessary cache test

--- test/transformation/cache_test.cc
+++ test/transformation/cache_test.cc
@@ -60,45 +60,6 @@ TEST_F(CacheTest, hit) {
   EXPECT_EQ(result.data(), result2.data());
 }
 
-TEST_F(CacheTest, notHitWithDifferentVaraible) {
-  std::unique_ptr<Transformation::TransformBase> trans = std::make_unique<LowerCase>();
-  Common::Variant data = "Hello, World!";
-
-  Common::EvaluateResults::Element transform_buffer(data, "");
-  Variable::Tx variable(std::string("test"), std::nullopt, false, false);
-  bool ret = trans->evaluate(*t_, &variable, transform_buffer, transform_buffer);
-  EXPECT_TRUE(ret);
-  std::string_view result = std::get<std::string_view>(transform_buffer.variant_);
-  EXPECT_EQ(result, "hello, world!");
-
-  Common::EvaluateResults::Element transform_buffe2(data, "");
-  Variable::Tx variable2(std::string("test2"), std::nullopt, false, false);
-  ret = trans->evaluate(*t_, &variable2, transform_buffe2, transform_buffe2);
-  EXPECT_TRUE(ret);
-  std::string_view result2 = std::get<std::string_view>(transform_buffe2.variant_);
-  EXPECT_EQ(result, result2);
-  EXPECT_NE(result.data(), result2.data());
-}
-
-TEST_F(CacheTest, notHitWithLessThanThreshold) {
-  std::unique_ptr<Transformation::TransformBase> trans = std::make_unique<LowerCase>();
-  std::string_view test_data = "Hello, World!";
-  Common::Variant data = test_data;
-
-  Common::EvaluateResults::Element transform_buffer(data, "");
-  Variable::Tx variable(std::string("test"), std::nullopt, false, false);
-  bool ret = trans->evaluate(*t_, &variable, transform_buffer, transform_buffer);
-  EXPECT_TRUE(ret);
-  std::string_view result = std::get<std::string_view>(transform_buffer.variant_);
-  EXPECT_EQ(result, "hello, world!");
-
-  Common::EvaluateResults::Element transform_buffe2(data, "");
-  ret = trans->evaluate(*t_, &variable, transform_buffe2, transform_buffe2);
-  EXPECT_TRUE(ret);
-  std::string_view result2 = std::get<std::string_view>(transform_buffe2.variant_);
-  EXPECT_NE(result.data(), result2.data());
-}
-
 // Issues #24
 TEST_F(CacheTest, getDuplicateArgsCache) {
   std::unique_ptr<Transformation::TransformBase> url_trans = std::make_unique<UrlDecode>();
